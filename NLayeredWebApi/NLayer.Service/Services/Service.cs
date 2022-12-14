e9d7fab92e12afbae10037b9f862f69cbc8e9922
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-12-14 21:00:03 +0300

    update

---
+++ NLayeredWebApi/NLayer.Service/Services/Service.cs
@@ -0,0 +1,53 @@
+﻿using NLayer.Core.Services;
+using System.Linq.Expressions;
+
+namespace NLayer.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entitites)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> expresssion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entitites)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expresssion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
