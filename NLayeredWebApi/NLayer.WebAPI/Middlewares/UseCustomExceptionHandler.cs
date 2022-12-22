518f42c26e1c02c62420323e2f491445b68f4864
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-12-22 22:27:21 +0300

    error fixed

--- NLayeredWebApi/NLayer.WebAPI/Middlewares/UseCustomExceptionHandler.cs
+++ NLayeredWebApi/NLayer.WebAPI/Middlewares/UseCustomExceptionHandler.cs
@@ -1,10 +1,30 @@
-﻿namespace NLayer.API.Middlewares
+﻿using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.Dtos;
+using NLayer.Service.Exceptions;
+using System.Text.Json;
+
+namespace NLayer.API.Middlewares
 {
     public static class UseCustomExceptionHandler
     {
-        public static void UseCustomException()
+        public static void UseCustomException(this IApplicationBuilder app)
         {
-
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        _ => 500    // _ demek default demektir.
+                    };
+                    context.Response.StatusCode = statusCode;
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                });
+            });
         }
     }
 }
